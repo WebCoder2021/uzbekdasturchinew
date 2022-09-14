603708a737ae652d5a3541d72d194924c450f98d
webcoder2021 <futureprogrammer2021@gmail.com>
2022-09-14 20:53:17 +0500

    gfgfgf

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import handler404
 from django.conf.urls import handler404
-
+from django.urls import re_path as url
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
@@ -31,4 +32,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if not settings.DEBUG:
+    urlpatterns += url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    urlpatterns += url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 handler404 = "home.views.bad_request"
